d9550a858865a50503fdf9ff5fea9d934e92332c
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-24 21:22:18 +0000

    modified lib folder

---
+++ lib/top/universities.rb
@@ -0,0 +1,8 @@
+require "top/universities/version"
+
+module Top
+  module Universities
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/top/universities/version.rb
@@ -0,0 +1,5 @@
+module Top
+  module Universities
+    VERSION = "0.1.0"
+  end
+end
